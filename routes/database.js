239912d77a3e4e080809cc890cdd6c2da097326c
Cameron <celasky@live.com>
2013-12-09 19:37:15 -0800

    Updated database info

--- routes/database.js
+++ routes/database.js
@@ -1,9 +1,10 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-	host : 'localhost',
-	user : 'root',
-	password : 'default'
+
+	user : 'adminXACDnCs',
+	password : 'BSJxmUYgzAyS'
+	
 	});
 
 exports.initDatabase = function(req, res)
