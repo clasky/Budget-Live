fa159a655813388d7302fb77bb557b5da393941f
clasky <celasky@live.com>
2013-12-17 22:23:32 -0800

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var http = require('http');
 var path = require('path');
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('.html', require('jade').__express);
